ab5cc0ed4ef94030b5d7ec8f8e63de2ce6fb7011
robhowden-sudo <rob.howden@gmail.com>
2026-08-27 12:44:08 +0100

    Import HUD relaunch intent

--- glasses/src/main/java/com/inthesky/radar/glasses/MainActivity.java
+++ glasses/src/main/java/com/inthesky/radar/glasses/MainActivity.java
@@ -1,6 +1,7 @@
 package com.inthesky.radar.glasses;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
